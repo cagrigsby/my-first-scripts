1213f1e1ef0864bc3d3f33f9a6ec3ea175d93aeb
grica421 <72837828+grica421@users.noreply.github.com>
2021-03-01 18:44:36 -0600

    Update pwexpiry_bash.sh

--- pwexpiry_bash.sh
+++ pwexpiry_bash.sh
@@ -10,4 +10,4 @@ date3=$((date2 + 30))
 users=$(awk -F: '($3>=1000)&&($1!="nobody"){print $1}' /etc/passwd)
 
 OUT=$(sudo awk -F: '(($3 < $date3))' /etc/shadow | cut -d: -f1)
-echo "These are the users that have not change their passwords in the last 30 days: $OUT"
+echo "These are the users that have not change their passwords in the last 30 days: ${OUT:369}"
